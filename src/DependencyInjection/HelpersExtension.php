e2e75c9268f0f1b950dd2042fe9b5c96ee590df6
Maciej Adamski <m.adamski@ideacode.pl>
2018-02-06 11:12:58 +0100

    Configuration of HelpersExtension

---
+++ src/DependencyInjection/HelpersExtension.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Adamski\Symfony\HelpersBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+class HelpersExtension extends Extension {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container) {
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . "/../Resources/config"));
+        $loader->load("services.yml");
+    }
+}
