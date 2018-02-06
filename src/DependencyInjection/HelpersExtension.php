ff2213507d3e2bed43a1da88ba8b87f5fe8d3ecb
Maciej Adamski <m.adamski@ideacode.pl>
2018-02-06 12:40:10 +0100

    Next try of Bundle configuration

--- src/DependencyInjection/HelpersExtension.php
+++ src/DependencyInjection/HelpersExtension.php
@@ -13,6 +13,11 @@ class HelpersExtension extends Extension {
      * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container) {
+        $configuration = new Configuration();
+        $config = $this->processConfiguration($configuration, $configs);
+
+        $container->setParameter("helpers.mailer.default_sender", $config["default_sender"]);
+
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . "/../Resources/config"));
         $loader->load("services.yaml");
     }
